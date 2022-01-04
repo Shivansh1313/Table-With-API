b7f892cc92ff9444b6d1d8cc48d25e135b958c55
unknown <shivansh.chopra>
2022-01-04 14:04:56 +0530

    delete functionality added

--- src/BasicTable.jsx
+++ src/BasicTable.jsx
@@ -7,7 +7,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useSelector, useDispatch } from 'react-redux';
-import { getUsers } from './redux/Actions/Actions';
+import { getUsers, setUsers } from './redux/Actions/Actions';
+import Button from '@mui/material/Button';
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
@@ -30,17 +31,26 @@ export default function BasicTable() {
     console.log(state);
     return state?.getUsers;
   });
+  const handleEditClick = () => {};
 
+  const handleDeleteClick = (id) => {
+    const temp = getUsersState.tableData.filter((item) => {
+      return item.id !== id;
+    });
+    dispatch(setUsers(temp));
+
+    return temp;
+  };
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>ID</TableCell>
+            <TableCell align="left">Email</TableCell>
+            <TableCell align="left">First Name&nbsp;</TableCell>
+            <TableCell align="left">Last Name&nbsp;</TableCell>
+            <TableCell align="left">Avatar Link&nbsp;</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -49,12 +59,20 @@ export default function BasicTable() {
               key={row.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row.id}
+              <TableCell align="left">{row.id}</TableCell>
+              <TableCell align="left">{row.email}</TableCell>
+              <TableCell align="left">{row.first_name}</TableCell>
+              <TableCell align="left">{row.last_name}</TableCell>
+              <TableCell align="left">{row.avatar}</TableCell>
+              <TableCell align="left">
+                <Button onClick={() => handleEditClick(row.id)}> Edit</Button>
               </TableCell>
-              <TableCell align="right">{row.first_name}</TableCell>
-              <TableCell align="right">{row.last_name}</TableCell>
-              <TableCell align="right">{row.avatar}</TableCell>
+              <TableCell align="left">
+                <Button onClick={() => handleDeleteClick(row.id)}>
+                  {' '}
+                  Delete
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
